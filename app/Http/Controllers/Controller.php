b5fd19da118d3559135f081db73a5c4840bdd53d
Thales Bento <dev.tbarbosa.bento@gmail.com>
2023-04-28 16:15:01 -0300

    Controller + route and form request

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ExampleRequest;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function submit(ExampleRequest $request)
+    {
+        return back()->withInput()->with('success',true);
+    }
 }
